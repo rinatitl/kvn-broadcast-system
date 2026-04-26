a8168d50c274bcefcb857ad40bfd58f18ad460a5
Rinat Farkhutdinov <Rinat.it2016@mail.ru>
2026-04-26 22:28:45 +0300

    fix: sync gameTitle properly

--- bundles/kvn-scoreboard/extension.js
+++ bundles/kvn-scoreboard/extension.js
@@ -10,8 +10,6 @@ module.exports = function (nodecg) {
 	const generalRep = nodecg.Replicant("general-information", "kvn-file-upload");
 
 	generalRep.on("change", (newVal) => {
-		if (newVal && newVal.gameTitle) {
-			scoreboardStatusRep.value.gameTitle = newVal.gameTitle;
-		}
+		scoreboardStatusRep.value.gameTitle = newVal?.gameTitle || "";
 	});
 };
